007bde3ac1b7fdd8f31b9e6e72ce02266ed388ec
MarkC-b3d <blendedmarks@gmail.com>
2022-08-16 16:54:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-from dearpygui import core,simple
-import subprocess
+import dearpygui.dearpygui as dpg
 import configparser
 import os
-
-##Basic configuration
-
+import subprocess
 config = configparser.ConfigParser()
 path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
 config.read(os.path.join(path, 'config.ini'))
-##User specifies where youtube dl is installed to depending on operating system
-
+font = (os.path.join(path, 'Gruppo-Regular.ttf'))
 installdir = config['youtubedlinstall']['path']
 
-core.set_main_window_title("YouTube Downloader V0.1")
-core.set_theme("Cherry") #cherry theme looks pretty cool
 
-def myfunction(sender,data):
-    youtubelink = (core.get_value("YouTube Link"))
+
+print (installdir)
+
+def downloadmp3():
+    youtubelink = (dpg.get_value("link"))
     subprocess.Popen(f'{installdir} -x --audio-format mp3 'f'{youtubelink}') #f-strings allow the passing of variables to a command
 
-##youtube download
-with simple.window("YouTube Downloader", width=600, height=200, no_close = True):
-    core.add_input_text(name="YouTube Link", on_enter=True, callback=myfunction)
-    core.add_button("Click here..", callback = myfunction)
-    core.set_main_window_size(1200,600)
-core.start_dearpygui()
+
+dpg.create_context()
+with dpg.font_registry():
+    # first argument ids the path to the .ttf or .otf file
+    default_font = dpg.add_font(font, 18)
+    dpg.bind_font(default_font)
+dpg.create_viewport(title='Custom Title', width=1280, height=720)
+
+with dpg.window(label="Twip Mix Maker", width=640, height=360, no_close=True):
+    dpg.add_text("Youtube Downloader")
+    link = dpg.add_input_text(label="YouTube URL", default_value="", tag="link")
+    dpg.add_button(label="Save", callback=downloadmp3)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
